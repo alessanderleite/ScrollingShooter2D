b5205cd2720151e368c4fba35f3460282ec4a853
alessanderleite <alessander.logistica@gmail.com>
2019-01-06 14:54:36 -0200

    code the Viewport class

--- app/src/main/java/br/com/alessanderleite/scrollingshooter2d/Viewport.java
+++ app/src/main/java/br/com/alessanderleite/scrollingshooter2d/Viewport.java
@@ -1,4 +1,37 @@
 package br.com.alessanderleite.scrollingshooter2d;
 
+import android.graphics.PointF;
+import android.graphics.RectF;
+
 public class Viewport {
+
+    private PointF currentViewportWorldCentre;
+    private RectF convertedRect;
+    private PointF convertedPoint;
+    private int pixelsPerMetreX;
+    private int pixelsPerMetreY;
+    private int screenCentreX;
+    private int screenCentreY;
+    private int metresToShowX;
+    private int metresToShowY;
+
+    // All the rest of the Viewport code goes here
+    public Viewport(int screenXResolution, int screenYResolution) {
+
+        screenCentreX = screenXResolution / 2;
+        screenCentreY = screenYResolution / 2;
+
+        pixelsPerMetreX = screenXResolution / 90;
+        pixelsPerMetreY = screenYResolution / 55;
+
+        metresToShowX = 92;
+        metresToShowY = 57;
+
+        convertedRect = new RectF();
+        convertedPoint = new PointF();
+
+        currentViewportWorldCentre = new PointF();
+
+        // End of viewport class
+    }
 }
