e582ec19ccedf6de0a777171cc1eba7bbc12d59f
alessanderleite <alessander.logistica@gmail.com>
2019-01-06 11:31:01 -0200

    initial commit

---
+++ app/src/main/java/br/com/alessanderleite/scrollingshooter2d/MainActivity.java
@@ -0,0 +1,12 @@
+package br.com.alessanderleite.scrollingshooter2d;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
