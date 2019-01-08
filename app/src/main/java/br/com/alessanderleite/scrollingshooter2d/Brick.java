b9e6dd40b8a384ece347320382ac7a2d4742a098
alessanderleite <alessander.logistica@gmail.com>
2019-01-08 07:58:09 -0200

    declare the objects and the variables to the Brick class

--- app/src/main/java/br/com/alessanderleite/scrollingshooter2d/Brick.java
+++ app/src/main/java/br/com/alessanderleite/scrollingshooter2d/Brick.java
@@ -1,4 +1,19 @@
 package br.com.alessanderleite.scrollingshooter2d;
 
+import android.graphics.RectF;
+
+import java.util.Random;
+
 public class Brick {
+
+    private RectF rect;
+    Random random = new Random();
+
+    boolean destroyed;
+
+    private boolean isRight;
+    private boolean isLeft;
+    private boolean isTop;
+    private int color;
+    private boolean clipped;
 }
