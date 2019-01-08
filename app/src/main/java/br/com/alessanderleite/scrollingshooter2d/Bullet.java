bcecb1d3ebb88ed3960390af9ef00f5b4aa6931f
alessanderleite <alessander.logistica@gmail.com>
2019-01-08 07:23:36 -0200

    code the shoot, update and getters methods to the Bullet class

--- app/src/main/java/br/com/alessanderleite/scrollingshooter2d/Bullet.java
+++ app/src/main/java/br/com/alessanderleite/scrollingshooter2d/Bullet.java
@@ -26,4 +26,40 @@ public class Bullet {
         // World coordinates contained in a point
         point = new PointF();
     }
+
+    public boolean shoot(float startX, float startY, float direction) {
+        if (!isActive) {
+            point.x = startX;
+            point.y = startY;
+
+            // How much to move horizontally, each frame
+            horizontalVelocity = (float)(Math.cos(Math.toRadians(direction)));
+
+            // How much to move vertically, each frame
+            verticalVelocity = (float)(Math.sin(Math.toRadians(direction)));
+
+            isActive = true;
+            return true;
+        }
+        // Bullet already active
+        return false;
+    }
+
+    public void update(long fps) {
+        // Move the bullet
+        point.x = point.x + horizontalVelocity * speed / fps;
+        point.y = point.y + verticalVelocity * speed / fps;
+    }
+
+    public PointF getPoint() {
+        return point;
+    }
+
+    public boolean getStatus() {
+        return isActive;
+    }
+
+    public void setInactive() {
+        isActive = false;
+    }
 }
