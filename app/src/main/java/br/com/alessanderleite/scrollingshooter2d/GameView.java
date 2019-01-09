a5568469f271363e1a241f4c814e55050696790c
alessanderleite <alessander.logistica@gmail.com>
2019-01-09 20:56:35 -0200

    create the GameView class

---
+++ app/src/main/java/br/com/alessanderleite/scrollingshooter2d/GameView.java
@@ -0,0 +1,18 @@
+package br.com.alessanderleite.scrollingshooter2d;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class GameView extends SurfaceView implements Runnable {
+
+    
+
+    public GameView(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
