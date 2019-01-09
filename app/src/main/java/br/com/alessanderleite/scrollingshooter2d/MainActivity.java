c28b43b768d7e90c5ad8e8d818d017c9d8357eb0
alessanderleite <alessander.logistica@gmail.com>
2019-01-09 08:00:55 -0200

    code the onCreate method to the MainActivity

--- app/src/main/java/br/com/alessanderleite/scrollingshooter2d/MainActivity.java
+++ app/src/main/java/br/com/alessanderleite/scrollingshooter2d/MainActivity.java
@@ -1,12 +1,31 @@
 package br.com.alessanderleite.scrollingshooter2d;
 
 import android.app.Activity;
+import android.graphics.Point;
+import android.graphics.PointF;
 import android.os.Bundle;
+import android.view.Display;
 
 public class MainActivity extends Activity {
+
+    // View will be the view of the game
+    // It will also hold the logic of the game
+    // and respond to screen touches as well
+    GameView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Get a Display object to access screen details
+        Display display = getWindowManager().getDefaultDisplay();
+
+        // Load the resolution into a Point object
+        Point size = new Point();
+        display.getSize(size);
+
+        // Initialize gameView and set it as the view
+        view = new GameView(this, size.x, size.y);
+        setContentView(view);
     }
 }
