ff8d95b139fa468e038574c7e7ce9a532c817792
alessanderleite <alessander.logistica@gmail.com>
2019-01-10 07:33:14 -0200

    code the run method to the GameView

--- app/src/main/java/br/com/alessanderleite/scrollingshooter2d/GameView.java
+++ app/src/main/java/br/com/alessanderleite/scrollingshooter2d/GameView.java
@@ -190,7 +190,27 @@ public class GameView extends SurfaceView implements Runnable {
 
     @Override
     public void run() {
+        while (playing) {
 
+            // Capture the current time in milliseconds in startFrameTime
+            long startFrameTime = System.currentTimeMillis();
+
+            // Update the frame
+            if (!paused) {
+                update();
+            }
+
+            // Draw the frame
+            draw();
+
+            // Calculate the fps this frame
+            // We can then use the result to
+            // time animations and more.
+            long timeThisFrame = System.currentTimeMillis() - startFrameTime;
+            if (timeThisFrame >= 1) {
+                fps = 1000 / timeThisFrame;
+            }
+        }
     }
 
     class HUD {
