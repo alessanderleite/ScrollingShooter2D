d0e8165e50e145c2f4cc9fba0c4b49f9ae3c3ff3
alessanderleite <alessander.logistica@gmail.com>
2019-01-11 22:46:05 -0200

    add the onResume ant the onPause methods to the MainActivity class

--- app/src/main/java/br/com/alessanderleite/scrollingshooter2d/MainActivity.java
+++ app/src/main/java/br/com/alessanderleite/scrollingshooter2d/MainActivity.java
@@ -28,4 +28,22 @@ public class MainActivity extends Activity {
         view = new GameView(this, size.x, size.y);
         setContentView(view);
     }
+
+    // This method executes when the player starts the game
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Tell the gameView resume method to execute
+        view.resume();
+    }
+
+    // This method executes when the player quits the game
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Tell the gameView pause method to execute
+        view.pause();
+    }
 }
